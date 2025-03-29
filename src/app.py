97433002888e03a8d6e4781393f3d251a895dd41
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-03-29 11:21:43 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", discord_url="https://google.com")
+    return render_template("index.html", discord_url="https://google.com", year=datetime.today().year)
 
 
 if __name__ == "__main__":
